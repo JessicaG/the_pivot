48f0aa18f65a6d7963d3d3986abb7dc735be5873
Jessica Goulding <jgoulding2@gmail.com>
2015-05-04 14:38:31 -0600

    Cleaned up formatting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,6 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show]
   before_action :item_rating, only: [:index, :show]
 
-	# def index
-  #   @items = Item.active
-  #   @categories = Category.all
-  # end
-  #
   def index
     if params[:search]
       @items = Item.search(params[:search]).order("created_at DESC")
@@ -21,10 +16,10 @@ class ItemsController < ApplicationController
   def search
     @items = Item.search params[:search]
   end
-
-	def show
+  
+  def show
     @categories = @item.categories
-	end
+  end
 
   private
 
